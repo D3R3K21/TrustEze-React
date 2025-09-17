e4e4db51b05ed44796ec723e980890014b5cc066
Derek <lucidsolutionspro@gmail.com>
2025-09-17 00:51:43 -0700

    added login page

--- UI/src/index.tsx
+++ UI/src/index.tsx
@@ -8,6 +8,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
+	  <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600&display=swap" rel="stylesheet"/>
+    <link href="https://fonts.cdnfonts.com/css/tt-tsars-a-trial" rel="stylesheet"/>
     <App />
   </React.StrictMode>
 );
