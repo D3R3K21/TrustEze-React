e4644de08d95dde44ef1f177974fdbfdd4a42b30
Britni <trusteze.britni@gmail.com>
2025-09-21 14:58:10 -0700

    checkin

--- UI/src/App.tsx
+++ UI/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import Navbar from './components/Layout/Navbar';
-import Footer from './components/Layout/Footer';
-import Login from './pages/Login';
-import Home from './pages/Home';
-import Search from './pages/Search';
-import Profile from './pages/Profile';
-import ProtectedRoute from './components/ProtectedRoute';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import "./App.css";
 
 function App() {
   return (
@@ -21,17 +21,16 @@ function App() {
               <Route path="/login" element={<Login />} />
               <Route path="/" element={<Home />} />
               <Route path="/search" element={<Search />} />
-              <Route 
-                path="/profile" 
+              <Route
+                path="/profile"
                 element={
                   <ProtectedRoute>
                     <Profile />
                   </ProtectedRoute>
-                } 
+                }
               />
             </Routes>
           </main>
-          <Footer />
         </div>
       </Router>
     </AuthProvider>
